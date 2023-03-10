d9a550bd4e9a4a3d2a14c976527e72ba6d23c235
Евгений <evgeniisergeev2002@list.ru>
2023-03-10 21:26:29 +0500

    add manual p2p mode

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import asyncio
 import secrets
 import socket
+import platform
 from contextlib import closing
 import stun
-from aiobtdht import DHT
-from aioudp import UDPServer
 from tcp_and_udp_proxy import udp_proxy
-
-bootstrap_nodes_hosts = [
-    ("dht.transmissionbt.com", 6881),
-    ("router.bittorrent.com", 6881),
-    ("router.bitcomet.com", 6881),
-    ("dht.aelitis.com", 6881),
-    ("bootstrap.jami.net", 4222)
-]
-
-
-async def test_method():
-    print("hello from async method")
 
 
 def find_free_port():
@@ -27,71 +14,44 @@ def find_free_port():
         return s.getsockname()[1]
 
 
-def get_random_infoHash():
-    return secrets.token_bytes(20)
-
-
 async def main(loop):
-    bootstrap_nodes = []
-    for (host, port) in bootstrap_nodes_hosts:
-        try:
-            bootstrap_nodes.append((socket.gethostbyname(host), port))
-        except:
-            pass
-
-    udp = UDPServer()
     ip = "0.0.0.0"
     port = find_free_port()
 
     print("Start getting external ip and port by stun server")
     nat_type, external_ip, external_port = stun.get_ip_info(ip, port)
-    print(f"{nat_type} {external_ip} {external_port}")
+    print(f"{nat_type} {external_ip}:{external_port}")
+    print(f"Send your ip and port to your friend - {external_ip}:{external_port}")
 
-    print(f"start server on ip: {ip} port: {port}")
-    udp.run(ip, port, loop=loop)
-    user_infohash = get_random_infoHash()
-    print(f"your infohash {user_infohash.hex()}")
+    print("Write your friend ip and port in format - ip:port")
+    dest_ip, dest_port = input().split()
+    dest_addr = (dest_ip, dest_port)
 
-    dht = DHT(int(user_infohash.hex(), 16), server=udp, loop=loop)
+    print("start sending data...")
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_DGRAM)  # UDP
+    pc_name = platform.node()
+    for _ in range(1000):
+        sock.sendto(str.encode(f"Привет от {pc_name}"), dest_addr)
+        time.sleep(1)
+        data, _ = sock.recvfrom(1024)
+        print("вам прислали: " + data.decode())
 
-    print("start bootstraping")
-    await dht.bootstrap(bootstrap_nodes)
-    print("bootstraping is done")
+    #print(f"start server on ip: {ip} port: {port}")
+    #peer_host = external_ip + ":" + str(external_port)
 
-    print(f"announce with port {external_port}")
-    await dht.announce(user_infohash, external_port)
-    print("announce done")
-
-    print("Write yours peer hash")
-    peer_infohash = bytes.fromhex(input())
-
-    print("search your peer in dht")
-    peers = []
-    step = 0
-    while len(peers) == 0:
-        step += 1
-        peers = await dht[peer_infohash]
-        if step % 10000 == 0:
-            print(f".", end=' ')
-
-    print(f"{len(peers)} peers founded")
-    print("peers:", peers)
-
-    peer_host = external_ip + ":" + str(external_port)
-
-    print("Select mode.")
-    print("1 - Server mode. You want share your server")
-    print("2 - Client mode. You want get access to server of other user")
-    mode = input()
-    if mode == 1:
-        print("Write source for proxing data 'ip:port' ")
-        real_server_host = input()
-        udp_proxy(peer_host, real_server_host)
-    elif mode == 2:
-        local_host = ip + ":" + str(port)
-        udp_proxy(local_host, peer_host)
-        print("Use this host to connect for your peer" + local_host)
-
+    #print("Select mode.")
+    #print("1 - Server mode. You want share your server")
+    #print("2 - Client mode. You want get access to server")
+    #mode = input()
+    #if mode == 1:
+    #    print("Write source for proxing data 'ip:port' ")
+    #    real_server_host = input()
+    #    udp_proxy(peer_host, real_server_host)
+    #elif mode == 2:
+    #    local_host = ip + ":" + str(port)
+    #    udp_proxy(local_host, peer_host)
+    #    print("Use this host to connect for your peer" + local_host)
 
 if __name__ == '__main__':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # remove on non windows system
